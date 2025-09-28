641229f5c11e349f9e195e957d912762676a5ebd
José Armando Melchor <jose.melchor@iteso.mx>
2025-09-28 01:33:06 -0600

    Commit 2

--- project/Funtions.py
+++ project/Funtions.py
@@ -1,3 +1,5 @@
+from libraries import *
+
 def librerias():
     import pandas as pd
     import numpy as np
@@ -5,13 +7,3 @@ def librerias():
 
 def primera_funcion():
     return print("Hola, esta es mi primera funcion")
-
-
-def segunda_funcion():
-    np.random.seed(0)
-    data = np.random.randn(1000)
-    plt.hist(data, bins=30, alpha=0.7, color='blue')
-    plt.title('Histograma de datos aleatorios')
-    plt.xlabel('Valor')
-    plt.ylabel('Frecuencia')
-    plt.show()
--- project/main.py
+++ project/main.py
@@ -1,6 +1,11 @@
-from Funtions import primera_funcion, librerias, segunda_funcion
+from libraries import *
+from funtions import primera_funcion, librerias, segunda_funcion
 
 
-primera_funcion()
-segunda_funcion()
+if __name__ == "__main__":
+    print("Hola, este es el archivo main.py")
+    primera_funcion()
+    print("El directorio actual es:", os.getcwd())
 
+
+    
